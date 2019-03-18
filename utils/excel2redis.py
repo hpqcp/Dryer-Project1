8bf31554034aca9a55b5424be4c988be259a15da
he <14499382@qq.com>
2019-03-18 17:25:36 +0800

    2

--- utils/excel2redis.py
+++ utils/excel2redis.py
@@ -1,7 +1,7 @@
 import redis as redis
-from pandas import  DataFrame
+from pandas import DataFrame
 import base.data_preProcess as bsPre
-
+import datetime
 
 # pool = redis.ConnectionPool(host='127.0.0.1')
 # r = redis.Redis(connection_pool=pool)
@@ -23,36 +23,36 @@ import base.data_preProcess as bsPre
 # exit()
 
 
-
-def fromCsv(): 
-    pool = redis.ConnectionPool(host='127.0.0.1', decode_responses=True)
-    r = redis.Redis(connection_pool=pool, decode_responses=True,db=1)
+def fromCsv():
+    pool = redis.ConnectionPool(host='127.0.0.1', decode_responses=True, db=1)
+    r = redis.Redis(connection_pool=pool, decode_responses=True, db=1)
     path = "d://hsj1.xlsx"
     group = "b"  # t test data
-    factoryID="9" # 1Ky 2 hy 3qy
-    deptID = "z" # z zs j jb c cx
-    lineID = "a" # line C
-    #batch = "0001"
-    #secID = 3 # 3 yslq 4 hs 5 yslq
+    factoryID = "9"  # 1Ky 2 hy 3qy
+    deptID = "z"  # z zs j jb c cx
+    lineID = "a"  # line C
+    # batch = "0001"
+    # secID = 3 # 3 yslq 4 hs 5 yslq
     df = bsPre.readExcel(path, 0)
     df = df[2853:]
-    df=df.reset_index(drop=True)
+    df = df.reset_index(drop=True)
     # print(df.values[0,:])
-    for j in range(0,df.shape[0],1):
-        data = df.values[j,:].tolist()
-        key = group+str(data[1])+str(data[0])+str(data[3])
-        r.rpush(key,data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],data[12],data[13],data[14])
+    for j in range(0, df.shape[0], 1):
+        data = df.values[j, :].tolist()
+        key = group +"-" + str(data[1]) +"-" +str(data[0]) +"-" + str(data[2])
+        r.rpush(key, data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11],
+                data[12], data[13], data[14])
 
 
 def getBatchData(_patten):
-    pool = redis.ConnectionPool(host='127.0.0.1',decode_responses=True)
-    r = redis.Redis(connection_pool=pool,decode_responses=True)
-    key1 = r.keys(pattern=_patten)#"t1zc0000*")
-    dt =DataFrame([r.lrange(key1[i],0,-1) for i in range(1, len(key1), 1)][:])
-    dt1=dt.sort_values(0)
-    dt2=dt1.reset_index(drop=True)
-    for i in range(1,dt2.shape[1],1):
-        dt2[[i]]=dt2[[i]].astype('float')
+    pool = redis.ConnectionPool(host='127.0.0.1', decode_responses=True)
+    r = redis.Redis(connection_pool=pool, decode_responses=True)
+    key1 = r.keys(pattern=_patten)  # "t1zc0000*")
+    dt = DataFrame([r.lrange(key1[i], 0, -1) for i in range(1, len(key1), 1)][:])
+    dt1 = dt.sort_values(0)
+    dt2 = dt1.reset_index(drop=True)
+    for i in range(1, dt2.shape[1], 1):
+        dt2[[i]] = dt2[[i]].astype('float')
     # print(dt.dtypes)
     r.connection_pool.disconnect()
     return dt2
