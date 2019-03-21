6a63b84750d932e838c3d2f425b15b6e5e09b41e
he <14499382@qq.com>
2019-03-21 09:05:18 +0800

    2

--- base/timeseries_process.py
+++ base/timeseries_process.py
@@ -92,9 +92,9 @@ def isSameBatch_Series(_series, _seriesDelay):
     return [startflag, endflag]
 
 
-# str = "b-YAR-19033102403-*"
+# str = "b-YAR-19033102503-*"
 # df = rds.getBatchData(str, 1)
-# df2 = rds.getBatchDataDelay(str, 15, 15, 1)
+# df2 = rds.getBatchDataDelay(str, 0, 0, 1)
 # series = pd.Series(df[0].values, index = df.index.values)
 # series2 = pd.Series(df2[0].values, index = df2.index.values)
 # print(isSameBatch_Series(series, series2))
