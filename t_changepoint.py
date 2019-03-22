67ba839432961247d5048e2349b701fad54debf3
he <14499382@qq.com>
2019-03-22 17:07:47 +0800

    2

--- t_changepoint.py
+++ t_changepoint.py
@@ -33,29 +33,39 @@ def kmeans_building(x1, x2, types_num, types, colors, shapes):
     return kmeans_model, x1_result, x2_result
 
 
-def getDifferenceList(_df, _stdGCount, _stdMax,_isPlot=False):
+def getDifferenceList(_df, _stdGCount,_stdMax, _isPlot=False):
     df = _df
     plt.figure(figsize=(8, 6))
     x1 = df.index  # x坐标列表
     result_list = []
-    for i in range(0,df.shape[1],1):
+    for i in range(0, df.shape[1], 1):
         x2 = df.values[:, i]  # y坐标列表
         colors = ['b', 'g', 'r']  # 颜色列表，因为要分3类，所以该列表有3个元素
         shapes = ['o', 's', 'D']  # 点的形状列表，因为要分3类，所以该列表有3个元素
         labels = ['A', 'B', 'C']  # 画图的标签内容，A, B, C分别表示三个类的名称
         kmeans_model, x1_result, x2_result = kmeans_building(x1, x2, 3, labels, colors, shapes)  # 本例要分3类，所以传入一个3
-        diff = [x2_result[i][-1] - x2_result[i][0] for i in range(0, len(x2_result), 1)]
+        diff = [abs(x2_result[i][-1] - x2_result[i][0]) for i in range(0, len(x2_result), 1)]
         maxIndex = diff.index(max(diff))  # 最大值index
         DifferenceList_x1 = x1_result[maxIndex]
         DifferenceList_x2 = x2_result[maxIndex]
         DifferenceSeries = pd.Series(DifferenceList_x2, index=DifferenceList_x1)
         # center = True
-        Difference_std = DifferenceSeries.rolling(_stdGCount).std()
+        # 移动标准差
+        # Difference_std = DifferenceSeries.rolling(_stdGCount).std()
+        # 移动极差
+        Difference_max = DifferenceSeries.rolling(_stdGCount).max()
+        Difference_min = DifferenceSeries.rolling(_stdGCount).min()
+        Difference_std = Difference_max.sub(Difference_min)
+
+        # _stdMax = (DifferenceSeries.max() - DifferenceSeries.min()) * 0.07
 
         Difference_std_sort = Difference_std.sort_values(ascending=False)
         Difference_std_top = Difference_std_sort[Difference_std_sort.values > _stdMax]
+        if (len(Difference_std_top) == 0):
+            Difference_std_top = Difference_std_sort
         Difference_std_topIndex = Difference_std_top.index
         Difference_std_topIndex = Difference_std_topIndex.sort_values()
+
         # rolling_series_list = []
         # for index in Difference_std_topIndex:
         #     rolling_series = DifferenceSeries[
@@ -79,13 +89,134 @@ def getDifferenceList(_df, _stdGCount, _stdMax,_isPlot=False):
         # cPlt.singlePlot(result_series.to_frame(name=None), _name="", _title="")
     return result_list
 
+def getDifferenceList_aut(_df, _stdGCount, _isPlot=False):
+    df = _df
+    plt.figure(figsize=(8, 6))
+    x1 = df.index  # x坐标列表
+    result_list = []
+    for i in range(0, df.shape[1], 1):
+        x2 = df.values[:, i]  # y坐标列表
+        colors = ['b', 'g', 'r']  # 颜色列表，因为要分3类，所以该列表有3个元素
+        shapes = ['o', 's', 'D']  # 点的形状列表，因为要分3类，所以该列表有3个元素
+        labels = ['A', 'B', 'C']  # 画图的标签内容，A, B, C分别表示三个类的名称
+        kmeans_model, x1_result, x2_result = kmeans_building(x1, x2, 3, labels, colors, shapes)  # 本例要分3类，所以传入一个3
+        diff = [abs(x2_result[i][-1] - x2_result[i][0]) for i in range(0, len(x2_result), 1)]
+        maxIndex = diff.index(max(diff))  # 最大值index
+        DifferenceList_x1 = x1_result[maxIndex]
+        DifferenceList_x2 = x2_result[maxIndex]
+        DifferenceSeries = pd.Series(DifferenceList_x2, index=DifferenceList_x1)
+        # center = True
+        # 移动标准差
+        # Difference_std = DifferenceSeries.rolling(_stdGCount).std()
+        # 移动极差
+        Difference_max = DifferenceSeries.rolling(_stdGCount).max()
+        Difference_min = DifferenceSeries.rolling(_stdGCount).min()
+        Difference_std = Difference_max.sub(Difference_min)
 
+        _stdMax = (DifferenceSeries.max() - DifferenceSeries.min()) * 0.07
 
+        Difference_std_sort = Difference_std.sort_values(ascending=False)
+        Difference_std_top = Difference_std_sort[Difference_std_sort.values > _stdMax]
+        if (len(Difference_std_top) == 0):
+            Difference_std_top = Difference_std_sort
+        Difference_std_topIndex = Difference_std_top.index
+        Difference_std_topIndex = Difference_std_topIndex.sort_values()
 
+        # rolling_series_list = []
+        # for index in Difference_std_topIndex:
+        #     rolling_series = DifferenceSeries[
+        #         (DifferenceSeries.index.values > index - _stdGCount) & (DifferenceSeries.index.values <= index)]
+        #     rolling_series_list.append(rolling_series)
+        #
+        # all_list = []
+        # [all_list.extend(_list) for _list in [rol.index.values for rol in rolling_series_list]]
+        #
+        # all_list = list(set(all_list))
+        # all_list.sort()
+        # result_series = DifferenceSeries[
+        #     (DifferenceSeries.index.values >= all_list[0]) & (DifferenceSeries.index.values <= all_list[-1])]
+
+        result_series = DifferenceSeries[(DifferenceSeries.index.values >= Difference_std_topIndex[0]) & (
+                DifferenceSeries.index.values <= Difference_std_topIndex[-1])]
+        result_list.append(result_series)
+        plt.scatter(result_series.index, result_series.values)
+        plt.legend()
+        plt.show()
+        # cPlt.singlePlot(result_series.to_frame(name=None), _name="", _title="")
+    return result_list
+
+# df 批次数据 _stdGCount 移动极差样本数 _stdTop 极差topN
+def getDifferenceList_Top(_df, _stdGCount, _stdTop, _isPlot=False):
+    df = _df
+    plt.figure(figsize=(8, 6))
+    x1 = df.index  # x坐标列表
+    result_list = []
+
+    for i in range(0, df.shape[1], 1):
+        x2 = df.values[:, i]  # y坐标列表
+        colors = ['b', 'g', 'r']  # 颜色列表，因为要分3类，所以该列表有3个元素
+        shapes = ['o', 's', 'D']  # 点的形状列表，因为要分3类，所以该列表有3个元素
+        labels = ['A', 'B', 'C']  # 画图的标签内容，A, B, C分别表示三个类的名称
+        kmeans_model, x1_result, x2_result = kmeans_building(x1, x2, 3, labels, colors, shapes)  # 本例要分3类，所以传入一个3
+        diff = [abs(x2_result[i][-1] - x2_result[i][0]) for i in range(0, len(x2_result), 1)]
+        maxIndex = diff.index(max(diff))  # 最大值index
+        DifferenceList_x1 = x1_result[maxIndex]
+        DifferenceList_x2 = x2_result[maxIndex]
+        DifferenceSeries = pd.Series(DifferenceList_x2, index=DifferenceList_x1)
+        # center = True
+        # 移动标准差
+        # Difference_std = DifferenceSeries.rolling(_stdGCount).std()
+        # 移动极差
+        Difference_max = DifferenceSeries.rolling(_stdGCount).max()
+        Difference_min = DifferenceSeries.rolling(_stdGCount).min()
+        Difference_std = Difference_max.sub(Difference_min)
+
+        Difference_std_sort = Difference_std.sort_values(ascending=False)
+        Difference_std_top = Difference_std_sort[0: _stdTop]
+
+        # top_index = []
+        # top_value = []
+        # i_stdTop = 0
+        # for i, t in Difference_std_top.to_frame(name=None).iteritems:
+        #     if (i_stdTop < _stdTop):
+        #         top_index.append(i)
+        #         top_value.append(t)
+        # Difference_std_top.pd.Series(top_value, index=top_index)
+
+        if (len(Difference_std_top) == 0):
+            Difference_std_top = Difference_std_sort
+
+        Difference_std_topIndex = Difference_std_top.index
+        Difference_std_topIndex = Difference_std_topIndex.sort_values()
+
+        rolling_series_list = []
+        for index in Difference_std_topIndex:
+            rolling_series = DifferenceSeries[
+                (DifferenceSeries.index.values > index - _stdGCount) & (DifferenceSeries.index.values <= index)]
+            rolling_series_list.append(rolling_series)
+
+        all_list = []
+        [all_list.extend(_list) for _list in [rol.index.values for rol in rolling_series_list]]
+
+        all_list = list(set(all_list))
+        all_list.sort()
+
+        # result_series = DifferenceSeries[
+        #     (DifferenceSeries.index.values >= all_list[0]) & (DifferenceSeries.index.values <= all_list[-1])]
+
+        result_series = DifferenceSeries[(DifferenceSeries.index.values >= Difference_std_topIndex[0]) & (
+                DifferenceSeries.index.values <= Difference_std_topIndex[-1])]
+        result_list.append(result_series)
+        plt.scatter(result_series.index, result_series.values)
+        plt.legend()
+        plt.show()
+        # cPlt.singlePlot(result_series.to_frame(name=None), _name="", _title="")
+        print()
+    return result_list
 
 
 # 批次
-batchStr = "b-YAR-19033102603-*"
+batchStr = "b-YAR-19033102703-*"
 # 获取批次数据
 df = rds.getBatchData(batchStr, 1)
 # #获取时间列
@@ -105,5 +236,8 @@ df = df[:int(len(df) / 2)]
 # cPlt.singlePlot(df,_title=batchStr)
 # cp = bsPre.compute_ChangePoint(df, _mode="last")
 # print(cp)
-cp1 = getDifferenceList(df, 8, 0.22)
+# df 批次数据 _stdGCount 移动极差样本数 _stdTop 极差topN
+# cp1 = getDifferenceList_Top(df, 5, 21)
+# cp1 = getDifferenceList(df, 5,1)
+cp1 = getDifferenceList_aut(df, 5)
 print(cp1)
