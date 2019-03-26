122dfe654c06ed0b2a78b67a8492e95d3c476782
he <14499382@qq.com>
2019-03-26 10:34:36 +0800

    2

--- base/data_transform.py
+++ base/data_transform.py
@@ -5,58 +5,78 @@ from pandas import DataFrame
 '''
 分段平均函数
 '''
-def splitMean(_series,_n):
-    return [sum(_series[i:i + _n])/_n for i in range(0, len(_series), _n)][:-1]
+
+
+def splitMean(_series, _n):
+    return [sum(_series[i:i + _n]) / _n for i in range(0, len(_series), _n)][:-1]
 
 
 '''dataframe移动(平均、中位数、最大、最小)
 '''
-def dataFrameRoll(_df,_freq,_colNum=[]):
+
+
+def dataFrameRoll(_df, _freq, _colNum=[]):
     colSize = _df.shape[1]
     rowSize = _df.shape[0]
-    if len(_colNum) <= 0 :
-        _colNum = range(0,colSize - 1,1)
+    if len(_colNum) <= 0:
+        _colNum = range(0, colSize - 1, 1)
     dfRtn = DataFrame()
     for i in _colNum:
         x1 = pd.Series(_df.values[:, i]).rolling(_freq).mean()
-        dfRtn[_df.columns[i]]  = x1
+        dfRtn[_df.columns[i]] = x1
     return dfRtn[_freq:]
 
 
 '''自动划分训练集、测试集
 '''
-def dataPartition(_df,_predictColNum,_partNum = 0.8,_mode="seq"):
-    if _partNum > 1 or _partNum<=0 :
-        return -1,-1,-1,-1
+
+
+def dataPartition(_df, _predictColNum, _partNum=0.8, _mode="seq"):
+    if _partNum > 1 or _partNum <= 0:
+        return -1, -1, -1, -1
     rowSize = _df.shape[0]
     colSize = _df.shape[1]
-    if rowSize <=1 :
-        return -2,-2,-2,-2
+    if rowSize <= 1:
+        return -2, -2, -2, -2
     fisrtNum = int(rowSize * _partNum)
-    valuesTest = _df.values[:,_predictColNum]
-    valuesLearn = _df.values[:,[j for j in range(0,colSize-1,1) if j != _predictColNum]]
-    return valuesLearn[:fisrtNum] , valuesLearn[fisrtNum :] ,valuesTest[:fisrtNum], valuesTest[fisrtNum :]
+    valuesTest = _df.values[:, _predictColNum]
+    valuesLearn = _df.values[:, [j for j in range(0, colSize - 1, 1) if j != _predictColNum]]
+    return valuesLearn[:fisrtNum], valuesLearn[fisrtNum:], valuesTest[:fisrtNum], valuesTest[fisrtNum:]
+
 
 '''生成数差（即后一个值减前一个值）
 '''
+
+
 def diff(_series):
-    return [_series[i+1] - _series[i]  for i in range(1, len(_series)-1, 1)][:]
+    return [_series[i + 1] - _series[i] for i in range(1, len(_series) - 1, 1)][:]
+
 
 '''生成数差（即后一个值减前一个值）
 '''
+
+
 def trend(_series):
-     return [2 if _series[i]<0 else 1  for i in range(0, len(_series)-1, 1)][:]
+    return [2 if _series[i] < 0 else 1 for i in range(0, len(_series) - 1, 1)][:]
+
 
 '''数据对齐
 '''
-def  data_alignment(_df,_putTimes):
-    dfLen = _df.shape[1]
+
+
+def data_alignment(_df, _putTimes):
+    dfC = _df.shape[1]
+    dfR = _df.shape[0]
     putLen = len(_putTimes)
     maxPut = max(_putTimes)
 
     rtnDF = DataFrame()
-    for i in range(0,dfLen,1):
-        dt = _df.values[_putTimes[i]:_putTimes[i] - maxPut,i]
-        rtnDF = rtnDF.append(pd.Series(dt),ignore_index=True)
+    for i in range(0, dfC, 1):
+        # dt = _df.values[_putTimes[i]:_putTimes[i] - maxPut,i]
+        dt = _df.loc[_putTimes[i]:dfR, i].reset_index(drop=True)
 
+        # rtnDF = rtnDF.append(pd.Series(dt),ignore_index=True)
+        rtnDF = rtnDF.join(dt, how='outer')
+    # print()
+    rtnDF = rtnDF[0:dfR - maxPut]
     return rtnDF
--- base/webSocketHelp.py
+++ base/webSocketHelp.py
@@ -3,22 +3,25 @@ from pandas.io.json import json_normalize
 import pandas as pd
 import json
 
+
 class WebSocketHelp:
     @staticmethod
-    def WebSocketJson(url,quary):
+    def WebSocketJson(url, quary):
         ws = create_connection(url)
         ws.send(quary)
-        result =  ws.recv()
+        result = ws.recv()
         ws.close()
         data_list = json.loads(result)
         if (data_list["ResultState"] == 1):
             datajson = json.loads(data_list["Message"])
-            data = [[d['TagName'], d['DateTime'], d['Value']] for d in datajson]
-            df = pd.DataFrame(data, columns=['TagName', 'DateTime', 'Value'])
+            data = [[d['TagName'], d['DateTime'], d['Value'],d['vValue']] for d in datajson]
+            df = pd.DataFrame(data, columns=['TagName', 'DateTime', 'Value','vValue'])
             return df
         else:
             return None
 
+
 if __name__ == "__main__":
-    df = WebSocketHelp.WebSocketJson("ws://10.130.65.207:8181/HisWeb","MES2RTDATA.U_Enriche_11010030005.DC_PH||2018-01-01 00:00:00||2018-01-01 01:00:00||Cyclic||6000")
+    df = WebSocketHelp.WebSocketJson("ws://10.130.65.207:8181/HisWeb",
+                                     "HZCF.U_Enriche_11400000170.DC_SSLL||2019-03-25 08:00:00||2019-03-25 16:00:00||Full||6000")
     print(df)
