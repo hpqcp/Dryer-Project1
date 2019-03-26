c740251081b60fa3a1bb790689b446f78900253b
hpqcp <14499382@qq.com>
2019-03-26 16:19:00 +0800

    1

--- t_line_c.py
+++ t_line_c.py
@@ -16,28 +16,32 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
 
 rf = RandomForestRegressor()
-# 批次
-batchStr = "t1zc0009*"
-# 获取批次数据
-df = rds.getBatchData(batchStr, 0)
-# #获取时间列
-# _series = pd.Series(df[0].values, index = df.index.values)
-# 获取数据是否连续
-# indexList=ts.check_ts_continuity(_series)
-# 定义使用的列
-useCol = [1, 5, 6, 7]  # , 10, 11, 12]
-df = DataFrame(df.values[:, useCol])
-diffCol = [0,1,2,3]
-# 目标列Y
-yCol = 3
-# 时间频率
-freq = 24
+dfAll = DataFrame()
+for i in range(0,7,1):
+    # 批次
+    batchStr = "t1zc0000*"
+    # 获取批次数据
+    df = rds.getBatchData(batchStr, 0)
+    # #获取时间列
+    # _series = pd.Series(df[0].values, index = df.index.values)
+    # 获取数据是否连续
+    # indexList=ts.check_ts_continuity(_series)
+    # 定义使用的列
+    useCol = [1, 5, 6,7,9]  # , 10, 11, 12]
+    df = DataFrame(df.values[:, useCol])
+    diffCol = [0,1,2,3,4]
+    # 目标列Y
+    yCol = 4
+    # 时间频率
+    freq = 6
+    # df = df[35:49]
+    putTimes = [0,90,136,361,390,420]
+    df = bsTrans.data_alignment(df,putTimes)
 
-# df = df[35:49]
-putTimes = [0,90,136,361,390,420]
-df = bsTrans.data_alignment(df,putTimes)
+
+
 cPlt.singlePlot(df, _title=batchStr)
-df = df[1000:5000]#int(len(df) / 4)]
+df = df[:]#int(len(df) / 4)]
 #原始
 xa,xb,ya,yb = bsTrans.dataPartition(df.iloc[:,diffCol],yCol)
 rf.fit(xa,ya)
@@ -61,13 +65,15 @@ d1= DataFrame({"T1":yb1,"P1":p1})
 d2= DataFrame({"T2":yb2,"P2":p2})
 c=[d,d1,d2]
 cName = ["Origin","rollMean","splitMean"]
-cPlt.pairPlot(c,cName,[0,16])
+cPlt.pairPlot(c,cName,[12,16])
 cPlt.pairPlot(c,cName)
 
-per20 = np.percentile(bsTrans.diff(df.values[:,[3]]),20)
-per80 = np.percentile(bsTrans.diff(df.values[:,[3]]),80)
-per50 = np.median(bsTrans.diff(df.values[:,[3]]))
-print(bsTrans.trend(dfSplite.values[:,[3]]))
+print(df.corr())
+
+# per20 = np.percentile(bsTrans.diff(df.values[:,[3]]),20)
+# per80 = np.percentile(bsTrans.diff(df.values[:,[3]]),80)
+# per50 = np.median(bsTrans.diff(df.values[:,[3]]))
+# print(bsTrans.trend(dfSplite.values[:,[3]]))
 
 
 # diff1 = DataFrame(bsTrans.diff(df.values[:,[1,3,5,7,9,11,13,15]]))
