791114617e72cdd4bde3312081f87600b44f3ab1
he <14499382@qq.com>
2019-03-27 10:01:30 +0800

    2

--- base/webSocketHelp.py
+++ base/webSocketHelp.py
@@ -29,6 +29,7 @@ class WebSocketHelp:
         return df
 
 
+
 if __name__ == "__main__":
     df = WebSocketHelp.WebSocketJson("ws://10.130.65.207:8181/HisWeb",
                                      "MES2RTDATA.U_DRY_11010160002.DC_PCH,MES2RTDATA.U_DRY_11010160002.DC_PH||2019-01-26 15:00:00||2019-01-26 17:30:00||Cyclic||360000")
