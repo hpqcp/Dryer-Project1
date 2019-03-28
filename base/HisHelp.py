40c1147fe1aac4dd6bea625566c657eb5675ae95
he <14499382@qq.com>
2019-03-28 11:29:08 +0800

    2

--- base/HisHelp.py
+++ base/HisHelp.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
-def RowToColumn(_df, _groupName, _vColumns):
+def RowToColumn(_df, _groupName, _vColumns,_indexName="index"):
     dfFL = pd.DataFrame()
     for name, group in _df.groupby(_groupName):
-        vDt = group.loc[:, _vColumns].reset_index(drop=True)
+        vDt =None
+        if(_indexName=="index"):
+            vDt = group.loc[:, _vColumns].reset_index(drop=True)
+        else:
+            vDt = pd.Series(group.loc[:, _vColumns].values, index=group.loc[:, _indexName].values)
         coName = name
         vDt.name = coName
         dfFL = dfFL.join(vDt, how='outer')
-        
+
     return dfFL
