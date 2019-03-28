c57c9e47d6308b7bf4894451e20f062db450f3de
he <14499382@qq.com>
2019-03-28 11:18:51 +0800

    2

---
+++ base/HisHelp.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def RowToColumn(_df, _groupName, _vColumns):
+    dfFL = pd.DataFrame()
+    for name, group in _df.groupby(_groupName):
+        vDt = group.loc[:, _vColumns].reset_index(drop=True)
+        coName = name
+        vDt.name = coName
+        dfFL = dfFL.join(vDt, how='outer')
+    return dfFL
