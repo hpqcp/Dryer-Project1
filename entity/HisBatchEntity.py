8c59a0fc45663e4fb2157615e44319857b172182
he <14499382@qq.com>
2019-04-02 15:22:13 +0800

    2

---
+++ entity/HisBatchEntity.py
@@ -0,0 +1,16 @@
+class HisBatchEntity:
+    PH = ""
+    PCH = ""
+    StartTime = ""
+    EndTime = ""
+    HisDf = None
+
+    def __init__(self, ):
+        self
+
+    def __init__(self, PCH, PH, StartTime, EndTime, HisDf):
+        self.PH = PH
+        self.PCH = PCH
+        self.StartTime = StartTime
+        self.EndTime = EndTime
+        self.HisDf = HisDf
--- test/t_line_c.py
+++ test/t_line_c.py
@@ -16,20 +16,22 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
 
 # 定义使用的列
-useCol = [1, 5, 6, 7, 8,9]  # , 10, 11, 12]
+useCol = [1, 5, 6]  # , 7, 9]  # , 10, 11, 12]
+# useCol = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 # df = DataFrame(df.values[:, useCol])
-diffCol = [0, 1, 2, 3, 4,5]
+diffCol = [0, 1, 2]  # , 3, 4]
 # 目标列Y
-yCol = 5
+yCol = 2
 # 时间频率
 freq = 6
 # 延时
-putTimes = [0, 90, 136, 361, 378, 420]
+putTimes = [0, 90, 136]  # , 361, 390, 420]
+# putTimes = [425, 440, 449, 0, 515, 561, 786, 804, 845]
 
 
-
-def MergeBatch(_batchList,_db):
+def MergeBatch(_batchList, _db):
     dfAll = DataFrame()
+    # writer = pd.ExcelWriter('C线10批数据（延时对齐）.xlsx')
     for _li in _batchList:
         batchStr = _li
         # 获取批次数据
@@ -39,18 +41,21 @@ def MergeBatch(_batchList,_db):
         # 获取数据是否连续
         # indexList=ts.check_ts_continuity(_series)
         df = DataFrame(df.values[:, useCol])
+        # [0, 15, 24, -425, 90, 136, 361, 379, 420]
         df = bsTrans.data_alignment(df, putTimes)
-        df = df[:]
+        # df = df[1500:5000]
         dfAll = dfAll.append(df)
-        # print(dfAll.shape[0])
-    dfAll = dfAll.reset_index(drop=True)
+        print(dfAll.shape[0])
+        # df.to_excel(writer, sheet_name=batchStr.replace('*', ''))
+    # writer.save()
+    # dfAll = dfAll.reset_index(drop=True)
     return dfAll
 
 
-batchList = ["t1zc0000*", "t1zc0001*", "t1zc0002*", "t1zc0003*", "t1zc0004*","t1zc0005*" ,"t1zc0006*","t1zc0007*","t1zc0008*","t1zc0009*"]
-allDf = MergeBatch(batchList,0)
-
-
+batchList = ["t1zc0000*", "t1zc0001*", "t1zc0002*", "t1zc0003*", "t1zc0004*", "t1zc0005*", "t1zc0006*", "t1zc0007*",
+             "t1zc0008*", "t1zc0009*"]
+allDf = MergeBatch(batchList, 0)
+# allDf.to_excel("合并.xlsx")
 rf = RandomForestRegressor()
 
 df = allDf
@@ -73,15 +78,14 @@ cPlt.singlePlot(df, _title=batchStr)
 df = df[:]  # int(len(df) / 4)]
 # 原始
 # xa, xb, ya, yb = bsTrans.dataPartition(df.iloc[:, diffCol], yCol)
-xa = df.iloc[:, [0,1,2,3,4]]
-ya = df.iloc[:,5]
+xa = df.iloc[:, [0, 1]]  # ,2,3]]
+ya = df.iloc[:, 2]
 df1 = rds.getBatchData("t1zc0000*", 0)
 df1 = DataFrame(df1.values[:, useCol])
 df1 = bsTrans.data_alignment(df1, putTimes)
-df1 = df1[:]
-xb = df1.iloc[:, [0,1,2,3,4]]
-yb = df1.iloc[:,5]
-
+df1 = df1[1500:5000]
+xb = df1.iloc[:, [0, 1]]  # ,2,3]]
+yb = df1.iloc[:, 2]
 
 rf.fit(xa, ya)
 p = rf.predict(xb)
@@ -90,11 +94,11 @@ print("MSE:", metrics.mean_squared_error(yb, p))
 # #移动平均
 dfRoll = bsTrans.dataFrameRoll(df, freq, diffCol)
 # xa1, xb1, ya1, yb1 = bsTrans.dataPartition(dfRoll, yCol)
-xa1 = dfRoll.iloc[:, [0,1,2,3,4]]
-ya1 = dfRoll.iloc[:,5]
+xa1 = dfRoll.iloc[:, [0, 1]]  # ,2,3]]
+ya1 = dfRoll.iloc[:, 2]
 dfRoll_pre = bsTrans.dataFrameRoll(df1, freq, diffCol)
-xb1 = dfRoll_pre.iloc[:, [0,1,2,3,4]]
-yb1 = dfRoll_pre.iloc[:,5]
+xb1 = dfRoll_pre.iloc[:, [0, 1]]  # ,2,3]]
+yb1 = dfRoll_pre.iloc[:, 2]
 rf.fit(xa1, ya1)
 p1 = rf.predict(xb1)
 print("MSE-roll:", metrics.mean_squared_error(yb1, p1))
@@ -114,7 +118,7 @@ cName = ["Origin", "rollMean", "splitMean"]
 cPlt.pairPlot(c, cName, [12, 16])
 cPlt.pairPlot(c, cName)
 
-
+print(df.corr())
 
 # per20 = np.percentile(bsTrans.diff(df.values[:,[3]]),20)
 # per80 = np.percentile(bsTrans.diff(df.values[:,[3]]),80)
