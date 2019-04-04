89f47a4b245a148593d9277ba69773708192e01e
he <14499382@qq.com>
2019-04-04 17:55:33 +0800

    2

--- bll/batchCutBLL.py
+++ bll/batchCutBLL.py
@@ -3,19 +3,8 @@ from base.webSocketHelp import WebSocketHelp
 import pandas as pd
 import base.HisHelp as his
 import datetime
+import bll.batch_process as batPro
 from entity.HisBatchEntity import HisBatchEntity
-
-
-# 行转列
-def RowToColumn(_df, _groupName, _vColumns):
-    dfFL = pd.DataFrame()
-    for name, group in _df.groupby(_groupName):
-        vDt = group.loc[:, _vColumns].reset_index(drop=True)
-        coName = name
-        vDt.name = coName
-        dfFL = dfFL.join(vDt, how='outer')
-    return dfFL
-
 
 FactoryCode = '1100'
 ZoneName = '制丝'
@@ -34,10 +23,11 @@ IdTypeDf = ms.GetIDInf(FactoryCode, ZoneName, BrandName, LineName, StageName, Id
 IdTypeSeries = pd.Series(IdTypeDf['TypeName'].values, index=IdTypeDf['GroupParameterTag'].values)
 # 先按小时获取牌号批次号
 IDTagName = ','.join(IdTypeSeries.index.values)
-startTime = "2019-01-02 00:00:00"
-endTime = "2019-01-04 00:00:00"
+startTime = "2019-01-02 00:00:00"  # 按时间段查询批次（开始时间）
+endTime = "2019-01-02 10:00:00"  # 按时间段查询批次（结束时间）
 hisSType = "Cyclic"
-frequency = "3600000"
+frequency = "3600000"  # 查询批次频率
+daraFrequency = "10000"  # 查询数据频率
 df = WebSocketHelp.WebSocketJson(webScortUrl,
                                  IDTagName + "||" + startTime + "||" + endTime + "||" + hisSType + "||" + frequency)
 # columns=df['TagName'].values, dtype=str
@@ -63,9 +53,9 @@ for name, group in dfFL.groupby('批次号实时点'):
     endTimeDt = endTimeDt + hoursDt
     oStartTime = startTimeDt.strftime("%Y-%m-%d %H:%M:%S")
     oEndTime = endTimeDt.strftime("%Y-%m-%d %H:%M:%S")
-    # 按照分钟获取数据
+
     batchIdDf = WebSocketHelp.WebSocketJson(webScortUrl,
-                                            IDTagName + "||" + oStartTime + "||" + oEndTime + "||Cyclic||60000")
+                                            IDTagName + "||" + oStartTime + "||" + oEndTime + "||Cyclic||" + daraFrequency)
 
     batchIdDf = his.RowToColumn(batchIdDf, 'TagName', 'vValue', _indexName='DateTime', _havIndex=True)
     c = IdTypeSeries[batchIdDf.columns.values.tolist()].values
@@ -114,7 +104,45 @@ for ImIndex, ImRow in BatchCutDf.iterrows():
     hisBatchEntity = HisBatchEntity(ImRow['PCH'], ImRow['PH'], ImRow['StartTime'], ImRow['EndTime'], StageBatchDf)
     hisBatchList.append(hisBatchEntity)
 
+# standardDf = pd.DataFrame({
+#     '切叶丝含水率': {'down': 20, 'up': 21},
+#     '薄板干燥出料含水率': {'down': 13, 'up': 15},
+#     '叶丝冷却出料含水率': {'down': 12.3, 'up': 13.3},
+# })
+# 工艺标准
+standardDf = pd.DataFrame({
+    1: {'down': 20, 'up': 21},
+    7: {'down': 13, 'up': 15},
+    9: {'down': 12.3, 'up': 13.3},
+})
+abnBatchParList = []#异常数据批次
+for _hisBatchEntity in hisBatchList:
+    hisDf = _hisBatchEntity.HisDf
+    hisDf = hisDf.astype('float64')#转换格式
+    useCol = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    hisDf.columns = useCol
+    point = batPro.check_batch_point(hisDf)  # 获取批次开始结束点
+    c = 1
+    # 按列进行遍历
+    for column, colSeries in hisDf.iteritems():#column:列名(参数点),colSeries:数据
+        # 判断该参数点是否有标准
+        if column in standardDf.columns:
+            up = standardDf.loc['up', column]
+            down = standardDf.loc['down', column]
+            p1 = point[c][0]
+            p2 = point[c][1]
+            wtDF = colSeries.values[p1:p2]  # serieas
+            # 错误1：索引有负数,错误2：料尾索引小于料头索引
+            wt = batPro.batch_Steadystate_r1(wtDF)  # 批次截取方法
+            # print(wt,len(wtDF),len(df))
+            wtSeries = colSeries[p1 + wt[0]:p1 + wt[1]]
+            # cPlt.singlePlot(df[p1 + wt[0]:p1 + wt[1]], _title=batchStr)
 
-print(hisBatchList)
-# 将异常批次数据存入redis
+            abnSeries = wtSeries[wtSeries > up & wtSeries < down]  # 过滤获取异常数据
+            if len(abnSeries) > 0:  # 如果存在超出标准的数据
+                # 毫厘牌号|毫厘牌号批次号|开始时间|结束时间|参数点
+                abnBatchParList.append(
+                    _hisBatchEntity.PH + "|" + _hisBatchEntity.PCH + "|" + hisBatchEntity.StartTime + "|" + hisBatchEntity.EndTime + "|" + column)
 
+        c = c + 1
+print(abnBatchParList)
