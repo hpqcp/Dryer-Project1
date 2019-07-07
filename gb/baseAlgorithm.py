fd675e7c1efdd4f318448a818547a93dbcb90faa
hpqcp <14499382@qq.com>
2019-07-08 01:09:30 +0800

    1

--- gb/baseAlgorithm.py
+++ gb/baseAlgorithm.py
@@ -1,4 +1,7 @@
 import gb.preProcess as pre
+import numpy as np
+import scipy.signal as sci
+import matplotlib.pyplot as pyplot
 
 
 #获取班次开始结束时间
@@ -14,20 +17,95 @@ def shiftTime(_shiftTag,_beginShiftTag,_endShiftTag,_beginTime,_endTime):
     hisData2 = hisData1[order]
     return hisData2
 
+#利用机器速度计算停机区间
+def haltIntervalBySpeed(_speedTag,_beginTime,_endTime):
+    tags = [_speedTag]
+    freq = "60000"  # 1分钟
+    hisData = pre.loadHisDataByCyclic(tags, freq, _beginTime, _endTime)
+    hisData.rename(columns={_speedTag: 'Speed'}, inplace=True)
+    his1 = hisData[hisData.iloc[:,1].astype(float) > 0]  # 开机
+    his2 = hisData[hisData.iloc[:,1].astype(float) <= 0]  # 停机
+    a= his1.index
+    b=his2.index
+    lsSec1 = continuousSegmentByIndex(his1.index)    #开机时段
+    lsSec2 = continuousSegmentByIndex(his2.index)    #停机时段
+    return  lsSec1,lsSec2
+
+#通过索引获取连续段
+def continuousSegmentByIndex(_index):
+    index1 = _index
+    ls1 =list()
+    g1 = index1[0]
+    for i in range(0,len(index1) - 1,1):
+        g2 = index1[i]
+        if (g2 + 1 == index1[i+1]):
+            continue
+        else:
+            g3 = index1[i]
+            ls1.append([g1,g3,g3-g1+1])
+            g1 = index1[i+1]
+    ls1.append([g1,index1[-1],index1[-1] - g1 + 1])
+    return  ls1
 
 
+#
+def findPeaksBySci(_hisData):
+    vector = _hisData.values[:, 1].astype(np.float)
+    indexes, values = sci.find_peaks(vector, height=7, distance=2.1)
+    #
+    print('Peaks are: %s' % (indexes))
+    pyplot.figure()
+    pyplot.plot(range(0, len(vector), 1), vector, 'r-')
+    y = _hisData.values[indexes, 1].astype('float')
+    pyplot.scatter(indexes, y)
+    pyplot.show()
+    #
+    return indexes
 
 
+#
+def shiftSection(_lsShifts):
+    ls1 = _lsShifts
+    r1 = _lsShifts[0]
+    loc=0
+    ls2=list()
+    for i in range(0,len(ls1) - 1,1):
+        if (r1 == ls1[i+1]):
+            continue
+        else:
+            ls2.append([loc,i])
+            r1 = ls1[i+1]
+            loc = i+1
+    ls2.append([loc,len(ls1) - 1])
+    return ls2
 
 
 
 
 
 if __name__ == "__main__":
-    # str = ["MES2RTDATA.U_Maker_11020030001.DC_SJCL","MES2RTDATA.U_Maker_11020030001.DC_BC",
-    #        "MES2RTDATA.U_Maker_11020030001.DC_YXSD","MES2RTDATA.U_Maker_11020030001.DC_PH"]
-    str1 ="MES2RTDATA.U_Maker_11020030001.DC_BC"
-    str2 ="MES2RTDATA.U_Maker_11020030001.DC_DQBCKSSJ"
-    str3 ="MES2RTDATA.U_Maker_11020030001.DC_DQBCJSSJ"
-    a = shiftTime(str1,str2,str3,"2019-07-01 06:00:00","2019-07-02 06:00:00")
-    print(a)
+    # str1 ="MES2RTDATA.U_Maker_11020030001.DC_BC"
+    # str2 ="MES2RTDATA.U_Maker_11020030001.DC_DQBCKSSJ"
+    # str3 ="MES2RTDATA.U_Maker_11020030001.DC_DQBCJSSJ"
+    # a = shiftTime(str1,str2,str3,"2019-07-01 06:00:00","2019-07-02 06:00:00")
+    # print(a)
+
+    # a = haltIntervalBySpeed("MES2RTDATA.U_Maker_11020030001.DC_YXSD","2019-07-01 06:00:00","2019-07-02 06:00:00")
+    # print(a)
+
+    # freq = "60000"  # 1min
+    # for i in range(1,10,1):
+    #     tags = ['MES2RTDATA.U_Packer_1102003000'+str(i)+'.DC_TBSJCL']
+    #     hisData = pre.loadHisDataByCyclic(tags, freq, '2019-07-4 6:00:00','2019-07-5 6:00:00')
+    #     findPeaksBySci(hisData)
+
+    freq = "60000"  # 1min
+    str1 = "MES2RTDATA.U_Maker_11020030001.DC_BC"
+    tags=[str1]
+    hisData = pre.loadHisDataByCyclic(tags, freq, '2019-07-4 6:00:00', '2019-07-5 6:00:00')
+    a = shiftSection(hisData.values[:,1])
+    print (a)
+
+    tags = ['MES2RTDATA.U_Packer_11020030001.DC_SJCL']
+    hisData = pre.loadHisDataByCyclic(tags, freq, '2019-07-4 6:00:00','2019-07-5 6:00:00')
+    findPeaksBySci(hisData)
--- test/gb-t1.py
+++ test/gb-t1.py
@@ -10,13 +10,13 @@ import base.change_point as cp
 
 df = ws.WebSocketHelp.WebSocketJson("ws://10.130.65.207:8181/HisWeb",
                                      "MES2RTDATA.U_Maker_11020030001.DC_SJCL"
-                                     "||2019-06-27 6:00:00||2019-06-28 6:00:00||Cyclic||60000")
+                                     "||2019-07-1 6:00:00||2019-07-2 6:00:00||Cyclic||60000")
 df1 = ws.WebSocketHelp.WebSocketJson("ws://10.130.65.207:8181/HisWeb",
                                      "MES2RTDATA.U_Maker_11020030001.DC_BC"
-                                     "||2019-06-27 6:00:00||2019-06-28 6:00:00||Cyclic||3600000")
+                                     "||2019-07-1 6:00:00||2019-07-2 6:00:00||Cyclic||3600000")
 df2 = ws.WebSocketHelp.WebSocketJson("ws://10.130.65.207:8181/HisWeb",
                                      "MES2RTDATA.U_Maker_11020030001.DC_YXSD"
-                                     "||2019-06-27 6:00:00||2019-06-28 6:00:00||Cyclic||60000")
+                                     "||2019-07-1 6:00:00||2019-07-2 6:00:00||Cyclic||60000")
 # df = ws.WebSocketHelp.WebSocketJson("ws://10.130.65.207:8181/HisWeb",
 #                                      "MES2RTDATA.U_Maker_11020030001.DC_JTH,MES2RTDATA.U_Maker_11020030001.DC_PH,MES2RTDATA.U_Maker_11020030001.DC_BC,"
 #                                      "MES2RTDATA.U_Maker_11020030001.DC_YXSD,MES2RTDATA.U_Maker_11020030001.DC_LLCL,MES2RTDATA.U_Maker_11020030001.DC_SJCL,"
@@ -34,14 +34,14 @@ df2 = ws.WebSocketHelp.WebSocketJson("ws://10.130.65.207:8181/HisWeb",
 vector = df.values[:,2].astype(np.float)
 vector1=df1.values[:,2].astype(np.float)
 vector2=df2.values[:,2].astype(np.float)
-# indexes  = sci.find_peaks_cwt(df.values[:,2].astype(np.float),np.arange(1, 4),max_distances=np.arange(1, 4)*2)
+#indexes  = sci.find_peaks_cwt(df.values[:,2].astype(np.float),np.arange(1, 4),max_distances=np.arange(1, 4)*2)
 
-# indexes = sci.argrelextrema(df.values[:,2].astype(np.float),comparator=np.greater,order=2)
+#indexes = sci.argrelextrema(df.values[:,2].astype(np.float),comparator=np.greater,order=2)
 
 indexes, _ = sci.find_peaks(vector, height=7, distance=2.1)
 print('Peaks are: %s' % (indexes))
 
-y = df.values[indexes,2]
+y = df.values[indexes,2].astype('float')
 
 # vector = [vector,df1.values[:,2].astype(np.float)]
 pyplot.figure()
